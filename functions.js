941bfefc6e3846738b400f7b6b99e4b58d48472b
Pakzone1 <115553017+Pakzone1@users.noreply.github.com>
2025-01-09 16:50:35 +0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,7 +1,7 @@
 const POLLING_INTERVAL = 1000;
 const MAX_RETRIES = 60;
 const moderators = new Set();
-let assistantKey = 'asst_ZMeUIKZN5fMsLXxxTO6r0DdI';
+let assistantKey = 'asst_8UxzIrQN6g6jJBYInmEnQpjv';
 const userThreads = {};
 const userMessages = {};
 const userMessageQueue = {};
